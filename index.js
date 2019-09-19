f768ac4399a90ea500a6007cdccc69e13db9cba4
ToujouAya <gracelord0705@gmail.com>
2019-09-20 00:51:00 +0700

    Hello

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const {
   parseSaveGame,
   MinionIdentityBehavior,
   AI_TRAIT_IDS,
-  AITraitsBehavior
+  AITraitsBehavior,
+  writeSaveGame,
+  MinionSkillGroupNames,
+  MinionResumeBehavior
 } = require("oni-save-parser");
 
-const { readFileSync } = require("fs");
+const { readFileSync, writeFileSync } = require("fs");
 
 function loadFile(fileName) {
   const fileData = readFileSync(`./test-data/${fileName}.sav`);
@@ -28,12 +31,42 @@ const gameObjects = saveData.gameObjects;
 const minions = saveData.gameObjects.find(x => x.name === "Minion");
 
 const CANDIDATE_TRAITS = AI_TRAIT_IDS.filter(x => x !== "None");
-
+let topTraits = {}
+let topApitudeBy = {}
 const minionsGameObjects = minions.gameObjects;
 for (const minion of minionsGameObjects) {
   const name = getMinionIdentifier(minion);
-  console.log(name);
+  const minionResume = getBehavior(minion, MinionResumeBehavior)
+  const {AptitudeBySkillGroup} = minionResume.templateData
+  if (name === 'Marie') {
+    topApitudeBy = AptitudeBySkillGroup
+  } else {
+    minionResume.templateData.AptitudeBySkillGroup = topApitudeBy
+  }
+  setAttrLvl(minion);
+  cloneTraits(minion, name);
 }
+saveFile(`${fileName}-001`, saveData)
+
+function setAttrLvl(minion) {
+  const skillBehavior = getBehavior(minion, AIAttributeLevelsBehavior);
+  // Set each attribute to 100
+  for (const attribute of skillBehavior.templateData.saveLoadLevels) {
+    attribute.level = 100;
+  }
+}
+
+function cloneTraits(minion, name) {
+  const traits = getBehavior(minion, AITraitsBehavior);
+  const { TraitIds } = traits.templateData;
+  if (name === 'Marie') {
+    topTraits = TraitIds;
+  }
+  else {
+    traits.templateData.TraitIds = topTraits;
+  }
+}
+
 function getMinionIdentifier(minion) {
   const minionIdentifier = getBehavior(minion, MinionIdentityBehavior);
   const templateData = minionIdentifier.templateData;
